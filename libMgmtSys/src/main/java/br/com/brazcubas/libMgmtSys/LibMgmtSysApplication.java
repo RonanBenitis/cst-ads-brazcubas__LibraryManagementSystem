09ea9661f76c0cf61f740d7e74f1ded52237dfa7
Ronan Ferraz <ronanbenitis@hotmail.com>
2024-04-29 07:50:12 -0300

    Starting commit

---
+++ libMgmtSys/src/main/java/br/com/brazcubas/libMgmtSys/LibMgmtSysApplication.java
@@ -0,0 +1,13 @@
+package br.com.brazcubas.libMgmtSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibMgmtSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibMgmtSysApplication.class, args);
+	}
+
+}
